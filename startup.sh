acb30d4e47559c96fc760b3d2f952f9a955ce235
MilkLiver <qooqaz2452@gmail.com>
2023-03-30 05:31:48 +0000

    add jupyterlab server

--- startup.sh
+++ startup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 echo "This is ansible controller contaiiner OwO!!" >>/etc/motd
 
+nohup jupyter lab --ip=0.0.0.0 --port 9527 --allow-root >>/var/log/jupyter_lab.log &
+
 /usr/sbin/sshd -Dp 22
